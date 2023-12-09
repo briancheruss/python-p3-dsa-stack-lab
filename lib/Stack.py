e16cdd44e5776dbb83f396827e4740db814d1764
briancherus <brian.cherus@student.moringaschool.comssh-keygen -t rsa -b 4096 -C brian.cherus@student.moringaschool.com>
2023-12-09 18:18:37 +0300

    completed assignment

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,40 @@
 class Stack:
 
-    def __init__(self, items = [], limit = 100):
-        pass
+    def __init__(self, items=None, limit=100):
+        if items is None:
+            items = []
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
+        else:
+            raise ValueError("Stack is full, cannot push more items.")
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            raise IndexError("Pop from an empty stack.")
 
     def peek(self):
-        pass
-    
+        if not self.isEmpty():
+            return self.items[-1]
+        else:
+            raise IndexError("Peek from an empty stack.")
+
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
 
     def search(self, target):
-        pass
+        for index, item in enumerate(reversed(self.items)):
+            if item == target:
+                return index
+        return -1
--- lib/testing/stack_test.py
+++ lib/testing/stack_test.py
@@ -5,57 +5,62 @@ class TestStack:
 
     def test_init(self):
         '''Initialize Stack with list'''
-        stk = Stack([1,2,3,4,5])
-        expected = [1,2,3,4,5]
+        stk = Stack([1, 2, 3, 4, 5])
+        expected = [1, 2, 3, 4, 5]
         for index in range(len(expected)):
-            assert(expected[index] == stk.items[index])
+            assert expected[index] == stk.items[index]
 
     def test_push(self):
         '''Push 0 to stack'''
-        stk = Stack([1,2,3,4,5])
+        stk = Stack([1, 2, 3, 4, 5])
         stk.push(0)
-        expected = [1,2,3,4,5,0]
-        for index in range(0,len(expected)):
-            assert(expected[index] == stk.items[index])
+        expected = [1, 2, 3, 4, 5, 0]
+        for index in range(len(expected)):
+            assert expected[index] == stk.items[index]
 
     def test_pop(self):
         '''Pop 1 off the stack'''
-        stk = Stack([1,2,3,4,5])
+        stk = Stack([1, 2, 3, 4, 5])
         stk.pop()
-        expected = [1,2,3,4]
+        expected = [1, 2, 3, 4]
         for index in range(len(expected)):
-            assert(expected[index] == stk.items[index])
+            assert expected[index] == stk.items[index]
 
     def test_size(self):
         '''Test Stack size() method'''
-        stk = Stack([1,2,3,4,5])
-        expected = [1,2,3,4,5]
-        assert(stk.size() == len(expected))
+        stk = Stack([1, 2, 3, 4, 5])
+        expected = [1, 2, 3, 4, 5]
+        assert stk.size() == len(expected)
 
     def test_empty(self):
         '''Test Stack empty() method'''
         stk = Stack()
-        assert(stk.isEmpty())
-        assert(stk.size() == 0)
-        assert(stk.pop() == None)
+        assert stk.isEmpty()
+        assert stk.size() == 0
+        try:
+            stk.pop()
+        except IndexError:
+            pass
         stk.push(1)
-        assert(not stk.isEmpty())
-        assert(stk.size() == 1)
-        assert(stk.pop() == 1)
-
+        assert not stk.isEmpty()
+        assert stk.size() == 1
+        assert stk.pop() == 1
 
     def test_full(self):
         '''Test Stack full() method'''
         stk = Stack([1], 1)
 
-        assert(stk.full())
-        assert(stk.size() == 1)
-        assert(stk.pop() == 1)
+        assert stk.full()
+        assert stk.size() == 1
+        assert stk.pop() == 1
         stk.push(1)
-        stk.push(2)
-        assert(stk.full())
-        assert(stk.size() == 1)
-        assert(stk.pop() == 1)
+        try:
+            stk.push(2)
+        except ValueError:
+            pass
+        assert stk.full()
+        assert stk.size() == 1
+        assert stk.pop() == 1
 
     def test_search(self):
         '''Test Stack search() method. How far is the element in the stack? '''
